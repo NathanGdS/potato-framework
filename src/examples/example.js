550d6ed77366f703a38899c178c934cf16d07df7
Nathan Santos <nathan.santos@finnet.com.br>
2023-03-10 17:23:11 -0300

    updating examples

--- src/examples/example.js
+++ src/examples/example.js
@@ -1,5 +1,6 @@
 import { HttpStatusCode } from "../constants/HttpStatusCode.constants.js";
 import { SweetPotatoApp } from "../index.js";
+import { loggerMiddleware } from "./middlewares/logger.middleware.js";
 import { registerRoutes } from "./routes/index.js";
 
 async function bootstrap() {
@@ -9,7 +10,7 @@ async function bootstrap() {
     
     registerRoutes(app); //loading routes from other files
 
-    app.get('example', async ({headers, queries, params}) => {
+    app.get('example', loggerMiddleware, async ({headers, queries, params}) => {
         // you can get the headers, body, query params and route params from request
         app.finishRequest(HttpStatusCode.SUCCESS,{ //default code=200
             received: {
@@ -21,5 +22,4 @@ async function bootstrap() {
     });
     app.listen();//default port is 8000
 }
-
 bootstrap();
---
+++ src/examples/middlewares/logger.middleware.js
@@ -0,0 +1,3 @@
+export async function loggerMiddleware(everthing) {
+    console.log(everthing);
+}
