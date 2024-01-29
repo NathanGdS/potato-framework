d539d99cffc56f1649e7d252fcca8fe76437d24f
Nathan Santos <nathan.santos@finnet.com.br>
2024-01-29 20:28:13 -0300

    feat: exporting all things at once in constans

--- package/constants/index.mjs
+++ package/constants/index.mjs
@@ -2,6 +2,10 @@ import { HttpMethod } from "./HttpMethod.constants.mjs";
 import { HttpStatusCode } from "./HttpStatusCode.constants.mjs";
 import { CONSTANTS_ROUTES } from "./routes.constants.mjs";
 
+export * from "./HttpMethod.constants.mjs";
+export * from "./HttpStatusCode.constants.mjs";
+export * from "./routes.constants.mjs";
+
 export const CONSTANTS = {
   defaultPort: 8000,
   methods: HttpMethod,
