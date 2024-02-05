396706a49305a5dbb09e1076e90d8efd4513363f
Nathan G. Santos <nathangar01@gmail.com>
2024-02-04 23:43:24 -0300

    feat: route resource function

--- package/index.mjs
+++ package/index.mjs
@@ -1,3 +1,23 @@
-import { SweetPotatoAppInstance } from "./SweetPotato.mjs";
+import { SweetPotato } from "./SweetPotato.mjs";
 
-export const SweetPotatoApp = SweetPotatoAppInstance;
+/**
+ * @class SweetPotatoApp
+ * @returns {SweetPotato}
+ */
+export class SweetPotatoApp {
+  #instance;
+  constructor() {
+    if (!this.#instance) {
+      this.#instance = new SweetPotato();
+    }
+    return this.#getInstance();
+  }
+
+  /**
+   *
+   * @returns {SweetPotato}
+   */
+  #getInstance() {
+    return this.#instance;
+  }
+}
